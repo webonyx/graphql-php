0af2fe79f2728d88d24618fa0fa47575e5d76a2c
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2017-11-28 12:28:54 +0700

    StandardServer: a bit more validation for parsed json PSR-7 request (related to #202)

--- src/Server/Helper.php
+++ src/Server/Helper.php
@@ -493,15 +493,20 @@ class Helper
                     );
                 }
 
+                // Try parsing ourselves if PSR-7 implementation doesn't parse JSON automatically
+                if (is_array($bodyParams) && empty($bodyParams)) {
+                    $bodyParams = json_decode($request->getBody(), true);
+
+                    if (json_last_error()) {
+                        throw new RequestError("Could not parse JSON: " . json_last_error_msg());
+                    }
+                }
+
                 if (!is_array($bodyParams)) {
                     throw new RequestError(
                         "GraphQL Server expects JSON object or array, but got " .
                         Utils::printSafeJson($bodyParams)
                     );
-                }
-
-                if (empty($bodyParams)) {
-                    $bodyParams = json_decode($request->getBody(), true);
                 }
             } else {
                 $bodyParams = $request->getParsedBody();
