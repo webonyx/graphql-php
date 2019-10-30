31cc867abb00f1d7fd7f32859143243325ed5cd2
Max Loeb <shmax@shmax.com>
2019-10-29 21:12:02 -0700

    undo change

--- src/Type/Schema.php
+++ src/Type/Schema.php
@@ -387,7 +387,7 @@ class Schema
      *
      * This operation requires full schema scan. Do not use in production environment.
      *
-     * @param UnionType|InterfaceType $abstractType
+     * @param InterfaceType|UnionType $abstractType
      *
      * @return Type[]
      *
