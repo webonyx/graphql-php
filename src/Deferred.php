c7aef1a54056b776a5d0dca79d48d9690ec2c78c
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2019-12-30 18:17:36 +0700

    More type hints for PHPStan

--- src/Deferred.php
+++ src/Deferred.php
@@ -8,11 +8,17 @@ use GraphQL\Executor\Promise\Adapter\SyncPromise;
 
 class Deferred extends SyncPromise
 {
+    /**
+     * @param callable() : mixed $executor
+     */
     public static function create(callable $executor) : self
     {
         return new self($executor);
     }
 
+    /**
+     * @param callable() : mixed $executor
+     */
     public function __construct(callable $executor)
     {
         parent::__construct($executor);
