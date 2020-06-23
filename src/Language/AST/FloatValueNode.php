1caf4a07e5514004c386b7d4be0ba351190dd59f
Martin Wernstål <m4rw3r@gmail.com>
2020-06-23 17:53:44 +0200

    fix: FloatValueNode actually contains floats

--- src/Language/AST/FloatValueNode.php
+++ src/Language/AST/FloatValueNode.php
@@ -9,6 +9,6 @@ class FloatValueNode extends Node implements ValueNode
     /** @var string */
     public $kind = NodeKind::FLOAT;
 
-    /** @var string */
+    /** @var float */
     public $value;
 }
