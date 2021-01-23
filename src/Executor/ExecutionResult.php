3b7433519ced55965e71633878019e0088913af8
Tamás Szigeti <tamas@szigeti.one>
2021-01-23 09:22:48 +0100

    Do not add errors key to result when empty (#766)
    * Do not add errors key when empty
    
    * Rename var, add comment
    
    * Use assertSame
    
    * Explicit empty array check
    
    Co-authored-by: Simon Podlipsky <simon@podlipsky.net>
    
    Co-authored-by: Simon Podlipsky <simon@podlipsky.net>

--- src/Executor/ExecutionResult.php
+++ src/Executor/ExecutionResult.php
@@ -142,10 +142,15 @@ class ExecutionResult implements JsonSerializable
                 return array_map($formatter, $errors);
             };
 
-            $result['errors'] = $errorsHandler(
+            $handledErrors = $errorsHandler(
                 $this->errors,
                 FormattedError::prepareFormatter($this->errorFormatter, $debug)
             );
+
+            // While we know that there were errors initially, they might have been discarded
+            if ($handledErrors !== []) {
+                $result['errors'] = $handledErrors;
+            }
         }
 
         if ($this->data !== null) {
