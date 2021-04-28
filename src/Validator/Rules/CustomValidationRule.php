f749fc8be303eaaf04a80d6669b0f71f7ebfad4d
Benedikt Franke <benedikt.franke@mll.com>
2021-04-28 14:21:10 +0200

    Type visitor related functions

--- src/Validator/Rules/CustomValidationRule.php
+++ src/Validator/Rules/CustomValidationRule.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace GraphQL\Validator\Rules;
 
-use GraphQL\Error\Error;
+use GraphQL\Language\AST\Node;
+use GraphQL\Language\VisitorOperation;
 use GraphQL\Validator\ValidationContext;
 
 class CustomValidationRule extends ValidationRule
@@ -12,19 +13,17 @@ class CustomValidationRule extends ValidationRule
     /** @var callable */
     protected $visitorFn;
 
-    public function __construct($name, callable $visitorFn)
+    /**
+     * @param callable(ValidationContext): (array<string, callable(Node): VisitorOperation|mixed|null>|array<string, array<string, callable(Node): VisitorOperation|mixed|null>>) $visitorFn
+     */
+    public function __construct(string $name, callable $visitorFn)
     {
         $this->name      = $name;
         $this->visitorFn = $visitorFn;
     }
 
-    /**
-     * @return Error[]
-     */
     public function getVisitor(ValidationContext $context)
     {
-        $fn = $this->visitorFn;
-
-        return $fn($context);
+        return ($this->visitorFn)($context);
     }
 }
