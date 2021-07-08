baaacab13404af6a0d2f19c183e994ba7f762e71
Simon Podlipsky <simon@podlipsky.net>
2021-07-08 13:50:54 +0300

    Update tests/Type/QueryPlanTest.php
    Co-authored-by: Benedikt Franke <benedikt@franke.tech>

--- tests/Type/QueryPlanTest.php
+++ tests/Type/QueryPlanTest.php
@@ -1026,7 +1026,7 @@ GRAPHQL;
         ];
 
         $hasCalled = false;
-        /** @var QueryPlan $queryPlan */
+        /** @var QueryPlan|null $queryPlan */
         $queryPlan = null;
 
         $query = new ObjectType(
