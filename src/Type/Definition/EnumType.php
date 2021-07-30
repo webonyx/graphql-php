a832c20a3dc7a40f5ca1bcbdf3fb8106c9b83194
Benedikt Franke <benedikt.franke@mll.com>
2021-07-30 08:12:34 +0200

    Throw SerializationError in EnumType

--- src/Type/Definition/EnumType.php
+++ src/Type/Definition/EnumType.php
@@ -8,6 +8,7 @@ use ArrayObject;
 use Exception;
 use GraphQL\Error\Error;
 use GraphQL\Error\InvariantViolation;
+use GraphQL\Error\SerializationError;
 use GraphQL\Language\AST\EnumTypeDefinitionNode;
 use GraphQL\Language\AST\EnumTypeExtensionNode;
 use GraphQL\Language\AST\EnumValueNode;
@@ -133,13 +134,6 @@ class EnumType extends Type implements InputType, OutputType, LeafType, Nullable
         return $this->values;
     }
 
-    /**
-     * @param mixed $value
-     *
-     * @return mixed
-     *
-     * @throws Error
-     */
     public function serialize($value)
     {
         $lookup = $this->getValueLookup();
@@ -147,7 +141,7 @@ class EnumType extends Type implements InputType, OutputType, LeafType, Nullable
             return $lookup[$value]->name;
         }
 
-        throw new InvariantViolation('Cannot serialize value as enum: ' . Utils::printSafe($value));
+        throw new SerializationError('Cannot serialize value as enum: ' . Utils::printSafe($value));
     }
 
     /**
@@ -166,13 +160,6 @@ class EnumType extends Type implements InputType, OutputType, LeafType, Nullable
         return $this->valueLookup;
     }
 
-    /**
-     * @param mixed $value
-     *
-     * @return mixed
-     *
-     * @throws Error
-     */
     public function parseValue($value)
     {
         $lookup = $this->getNameLookup();
@@ -183,13 +170,6 @@ class EnumType extends Type implements InputType, OutputType, LeafType, Nullable
         throw new Error('Cannot represent value as enum: ' . Utils::printSafe($value));
     }
 
-    /**
-     * @param mixed[]|null $variables
-     *
-     * @return null
-     *
-     * @throws Exception
-     */
     public function parseLiteral(Node $valueNode, ?array $variables = null)
     {
         if ($valueNode instanceof EnumValueNode) {
