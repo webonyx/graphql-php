60529b6c80d66632b8638b48e5ec732c33b75988
Benedikt Franke <benedikt.franke@mll.com>
2021-08-02 10:57:03 +0200

    wording

--- src/Error/SerializationError.php
+++ src/Error/SerializationError.php
@@ -7,7 +7,7 @@ namespace GraphQL\Error;
 use Exception;
 
 /**
- * Thrown when failing to serialize a scalar value.
+ * Thrown when failing to serialize a leaf value.
  *
  * Not generally safe for clients, as the wrong given value could
  * be something not intended to ever be seen by clients.
