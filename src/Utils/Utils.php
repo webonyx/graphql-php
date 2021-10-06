725340fde2992625f1e5c8c2eb43cb5e69582605
Benedikt Franke <benedikt.franke@mll.com>
2021-10-06 13:13:56 +0200

    Handle empty items in quoted or list (#957)

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -13,7 +13,6 @@ use GraphQL\Language\AST\Node;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\WrappingType;
 use InvalidArgumentException;
-use LogicException;
 use stdClass;
 use Traversable;
 
@@ -552,12 +551,12 @@ class Utils
     }
 
     /**
-     * @param string[] $items
+     * @param array<string> $items
      */
     public static function quotedOrList(array $items): string
     {
         $quoted = array_map(
-            static fn (string $item): string => sprintf('"%s"', $item),
+            static fn (string $item): string => "\"{$item}\"",
             $items
         );
 
@@ -565,12 +564,12 @@ class Utils
     }
 
     /**
-     * @param string[] $items
+     * @param array<string> $items
      */
     public static function orList(array $items): string
     {
         if (count($items) === 0) {
-            throw new LogicException('items must not need to be empty.');
+            return '';
         }
 
         $selected       = array_slice($items, 0, 5);
@@ -601,12 +600,11 @@ class Utils
      * as a single edit which helps identify mis-cased values with an edit distance
      * of 1
      *
-     * @param string   $input
-     * @param string[] $options
+     * @param array<string> $options
      *
-     * @return string[]
+     * @return array<int, string>
      */
-    public static function suggestionList($input, array $options): array
+    public static function suggestionList(string $input, array $options): array
     {
         $optionsByDistance = [];
         $threshold         = mb_strlen($input) * 0.4 + 1;
