6404b71fdc0f96409a09b0b21163a5eb383a9074
spawnia <benedikt@franke.tech>
2021-10-10 13:34:34 +0200

    Remove deprecated public property access to `FieldDefinition::$type`

--- tests/Type/DefinitionTest.php
+++ tests/Type/DefinitionTest.php
@@ -6,7 +6,6 @@ namespace GraphQL\Tests\Type;
 
 use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use GraphQL\Error\InvariantViolation;
-use GraphQL\Error\Warning;
 use GraphQL\Tests\Type\TestClasses\MyCustomType;
 use GraphQL\Tests\Type\TestClasses\OtherCustom;
 use GraphQL\Type\Definition\CustomScalarType;
@@ -195,57 +194,6 @@ class DefinitionTest extends TestCase
         $feedFieldType = $feedField->getType();
         self::assertInstanceOf('GraphQL\Type\Definition\ListOfType', $feedFieldType);
         self::assertSame($this->blogArticle, $feedFieldType->getWrappedType());
-    }
-
-    public function testFieldDefinitionPublicTypeGetDeprecation(): void
-    {
-        $fieldDef = FieldDefinition::create([
-            'type' => Type::string(),
-            'name' => 'GenericField',
-        ]);
-
-        Warning::setWarningHandler(static function ($message): void {
-            self::assertEquals($message, 'The public getter for \'type\' on FieldDefinition has been deprecated and will be removed in the next major version. Please update your code to use the \'getType\' method.');
-        });
-
-        self::assertFalse(isset($fieldDef->nonExistentProp));
-        $fieldDef->nonExistentProp = 'someValue';
-        self::assertTrue(isset($fieldDef->nonExistentProp));
-
-        // @phpstan-ignore-next-line type is private, but we're allowing its access temporarily via a magic method
-        $type = $fieldDef->type;
-    }
-
-    public function testFieldDefinitionPublicTypeSetDeprecation(): void
-    {
-        $fieldDef = FieldDefinition::create([
-            'type' => Type::string(),
-            'name' => 'GenericField',
-        ]);
-
-        Warning::setWarningHandler(static function ($message): void {
-            self::assertEquals($message, 'The public setter for \'type\' on FieldDefinition has been deprecated and will be removed in the next major version.');
-        });
-
-        // @phpstan-ignore-next-line type is private, but we're allowing its access temporarily via a magic method
-        $fieldDef->type = Type::int();
-
-        $fieldDef->nonExistentProp = 'someValue';
-        self::assertEquals($fieldDef->nonExistentProp, 'someValue');
-    }
-
-    public function testFieldDefinitionPublicTypeIssetDeprecation(): void
-    {
-        $fieldDef = FieldDefinition::create([
-            'type' => Type::string(),
-            'name' => 'GenericField',
-        ]);
-
-        Warning::setWarningHandler(static function ($message): void {
-            self::assertEquals($message, 'The public getter for \'type\' on FieldDefinition has been deprecated and will be removed in the next major version. Please update your code to use the \'getType\' method.');
-        });
-
-        isset($fieldDef->type);
     }
 
     /**
