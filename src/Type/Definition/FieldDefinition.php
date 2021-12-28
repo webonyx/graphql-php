321ad8a27ede47b00bd8f0871d79fb171fc1602f
spawnia <benedikt@franke.tech>
2021-12-28 22:15:24 +0100

    fix

--- src/Type/Definition/FieldDefinition.php
+++ src/Type/Definition/FieldDefinition.php
@@ -18,7 +18,7 @@ use function is_string;
  * @phpstan-import-type FieldResolver from Executor
  * @phpstan-import-type ArgumentListConfig from Argument
  * @phpstan-type FieldType (Type&OutputType)|callable(): (Type&OutputType)
- * @phpstan-type ComplexityFn callable(int, array<string, mixed>): int|null
+ * @phpstan-type ComplexityFn callable(int, array<string, mixed>): (int|null)
  * @phpstan-type FieldDefinitionConfig array{
  *     name: string,
  *     type: FieldType,
