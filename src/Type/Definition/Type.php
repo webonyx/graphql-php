3425a0378a8f80ec194e404a00912a81bfeca528
Edi Modrić <edi.modric@gmail.com>
2022-03-03 11:16:15 +0100

    Add ReturnTypeWillChange attribute to disable deprecation warnings on PHP 8.1

--- src/Type/Definition/Type.php
+++ src/Type/Definition/Type.php
@@ -359,6 +359,7 @@ abstract class Type implements JsonSerializable
     /**
      * @return string
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->toString();
