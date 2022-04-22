c04f084a3d639457cebcc9327253d96b4db69b31
Benedikt Franke <benedikt@franke.tech>
2022-04-22 15:05:45 +0200

    inline

--- src/Validator/Rules/UniqueDirectiveNames.php
+++ src/Validator/Rules/UniqueDirectiveNames.php
@@ -19,41 +19,41 @@ class UniqueDirectiveNames extends ValidationRule
     public function getSDLVisitor(SDLValidationContext $context): array
     {
         $schema = $context->getSchema();
+
         /** @var array<string, NameNode> $knownDirectiveNames */
         $knownDirectiveNames = [];
-        $checkDirectiveName = static function ($node) use ($context, $schema, &$knownDirectiveNames): ?VisitorOperation {
-            $directiveName = $node->name->value;
-
-            if ($schema !== null && $schema->getDirective($directiveName) !== null) {
-                $context->reportError(
-                    new Error(
-                        'Directive "@' . $directiveName . '" already exists in the schema. It cannot be redefined.',
-                        $node->name,
-                    ),
-                );
-
-                return null;
-            }
-
-            if (isset($knownDirectiveNames[$directiveName])) {
-                $context->reportError(
-                    new Error(
-                        'There can be only one directive named "@' . $directiveName . '".',
-                        [
-                            $knownDirectiveNames[$directiveName],
-                            $node->name,
-                        ]
-                    ),
-                );
-            } else {
-                $knownDirectiveNames[$directiveName] = $node->name;
-            }
-
-            return Visitor::skipNode();
-        };
 
         return [
-            NodeKind::DIRECTIVE_DEFINITION => $checkDirectiveName,
+            NodeKind::DIRECTIVE_DEFINITION => static function ($node) use ($context, $schema, &$knownDirectiveNames): ?VisitorOperation {
+                $directiveName = $node->name->value;
+
+                if ($schema !== null && $schema->getDirective($directiveName) !== null) {
+                    $context->reportError(
+                        new Error(
+                            'Directive "@' . $directiveName . '" already exists in the schema. It cannot be redefined.',
+                            $node->name,
+                        ),
+                    );
+
+                    return null;
+                }
+
+                if (isset($knownDirectiveNames[$directiveName])) {
+                    $context->reportError(
+                        new Error(
+                            'There can be only one directive named "@' . $directiveName . '".',
+                            [
+                                $knownDirectiveNames[$directiveName],
+                                $node->name,
+                            ]
+                        ),
+                    );
+                } else {
+                    $knownDirectiveNames[$directiveName] = $node->name;
+                }
+
+                return Visitor::skipNode();
+            },
         ];
     }
 }
