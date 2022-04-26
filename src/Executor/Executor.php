7bda0b16f2ee261f1e313097bfb2f1c8e208ad7a
Benedikt Franke <benedikt.franke@mll.com>
2022-04-26 10:56:11 +0200

    no null safe access

--- src/Executor/Executor.php
+++ src/Executor/Executor.php
@@ -189,7 +189,7 @@ class Executor
             }
         } elseif (is_object($objectValue)) {
             if (isset($objectValue->{$fieldName})) {
-                $property = $objectValue?->{$fieldName};
+                $property = $objectValue->{$fieldName};
             }
         }
 
