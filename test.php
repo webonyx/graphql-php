3dc3484c44edb09c8a6bd6239c5cf349023c48c8
Benedikt Franke <benedikt.franke@mll.com>
2022-11-04 10:56:59 +0100

    clean

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-assert(true, new \GraphQL\Utils\LazyException(function () {
-    echo 'foo';
-
-    return 'bar;';
-}));
