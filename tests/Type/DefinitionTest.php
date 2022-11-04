a54b44518004d8a40158ea4abdad8993059a5676
Benedikt Franke <benedikt.franke@mll.com>
2022-11-04 12:00:42 +0100

    Extract LazyDefinitionTest.php

--- tests/Type/DefinitionTest.php
+++ tests/Type/DefinitionTest.php
@@ -5,8 +5,6 @@ namespace GraphQL\Tests\Type;
 use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use GraphQL\Error\Error;
 use GraphQL\Error\InvariantViolation;
-use GraphQL\Executor\Executor;
-use GraphQL\Language\Parser;
 use GraphQL\Tests\TestCaseBase;
 use GraphQL\Tests\Type\TestClasses\MyCustomType;
 use GraphQL\Tests\Type\TestClasses\OtherCustom;
@@ -14,17 +12,13 @@ use GraphQL\Type\Definition\Argument;
 use GraphQL\Type\Definition\CustomScalarType;
 use GraphQL\Type\Definition\EnumType;
 use GraphQL\Type\Definition\EnumValueDefinition;
-use GraphQL\Type\Definition\FieldDefinition;
 use GraphQL\Type\Definition\InputObjectField;
 use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\InterfaceType;
-use GraphQL\Type\Definition\IntType;
 use GraphQL\Type\Definition\ListOfType;
 use GraphQL\Type\Definition\NamedType;
 use GraphQL\Type\Definition\NonNull;
 use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\ScalarType;
-use GraphQL\Type\Definition\StringType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\UnionType;
 use GraphQL\Type\Schema;
@@ -1746,11 +1740,10 @@ final class DefinitionTest extends TestCaseBase
             ],
         ]);
 
-        new Schema(
-            [
-                'query' => $QueryType,
-                'types' => [new \stdClass()], ]
-        );
+        new Schema([
+            'query' => $QueryType,
+            'types' => [new \stdClass()],
+        ]);
     }
 
     /**
@@ -1859,170 +1852,5 @@ final class DefinitionTest extends TestCaseBase
             'types' => [$FirstBadObject, $SecondBadObject],
         ]);
         $schema->assertValid();
-    }
-
-    public function testAllowsTypeWhichDefinesItFieldsAsClosureReturningFieldDefinitionAsArray(): void
-    {
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                'f' => static fn (): array => [
-                    'type' => Type::string(),
-                ],
-            ],
-        ]);
-
-        $objType->assertValid();
-
-        self::assertSame(Type::string(), $objType->getField('f')->getType());
-    }
-
-    public function testAllowsTypeWhichDefinesItFieldsAsClosureReturningFieldDefinitionAsObject(): void
-    {
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                'f' => static fn (): FieldDefinition => new FieldDefinition([
-                    'name' => 'f',
-                    'type' => Type::string(),
-                ]),
-            ],
-        ]);
-        $objType->assertValid();
-
-        self::assertSame(Type::string(), $objType->getField('f')->getType());
-    }
-
-    public function testAllowsTypeWhichDefinesItFieldsAsInvokableClassReturningFieldDefinitionAsArray(): void
-    {
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                'f' => new class() {
-                    /**
-                     * @return array{type: ScalarType}
-                     */
-                    public function __invoke(): array
-                    {
-                        return ['type' => Type::string()];
-                    }
-                },
-            ],
-        ]);
-        $objType->assertValid();
-
-        self::assertSame(Type::string(), $objType->getField('f')->getType());
-    }
-
-    public function testLazyFieldNotExecutedIfNotAccessed(): void
-    {
-        $resolvedCount = 0;
-        $fieldCallback = static function () use (&$resolvedCount): array {
-            ++$resolvedCount;
-
-            return ['type' => Type::string()];
-        };
-
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                'f' => $fieldCallback,
-                'b' => static function (): void {
-                    throw new \RuntimeException('Would not expect this to be called!');
-                },
-            ],
-        ]);
-
-        self::assertSame(Type::string(), $objType->getField('f')->getType());
-        self::assertSame(1, $resolvedCount);
-    }
-
-    public function testLazyFieldNotExecutedIfNotAccessedInQuery(): void
-    {
-        $resolvedCount = 0;
-        $lazyField = static function () use (&$resolvedCount): array {
-            ++$resolvedCount;
-
-            return ['type' => Type::string()];
-        };
-
-        $query = new ObjectType([
-            'name' => 'Query',
-            'fields' => [
-                'f' => $lazyField,
-                'b' => static function (): void {
-                    throw new \RuntimeException('Would not expect this to be called!');
-                },
-            ],
-        ]);
-
-        $schema = new Schema(['query' => $query]);
-        $result = Executor::execute($schema, Parser::parse('{ f }'));
-
-        self::assertSame(['f' => null], $result->data);
-        self::assertSame(1, $resolvedCount);
-    }
-
-    public function testLazyFieldsAreResolvedWhenValidatingType(): void
-    {
-        $resolvedCount = 0;
-        $fieldCallback = static function () use (&$resolvedCount): array {
-            ++$resolvedCount;
-
-            return ['type' => Type::string()];
-        };
-
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                'f' => $fieldCallback,
-                'o' => $fieldCallback,
-            ],
-        ]);
-        $objType->assertValid();
-
-        self::assertSame(Type::string(), $objType->getField('f')->getType());
-        self::assertSame(2, $resolvedCount);
-    }
-
-    public function testThrowsWhenLazyFieldDefinitionHasNoKeysForFieldNames(): void
-    {
-        $objType = new ObjectType([
-            'name' => 'SomeObject',
-            'fields' => [
-                static fn (): array => [
-                    'type' => Type::string(),
-                ],
-            ],
-        ]);
-
-        $this->expectExceptionObject(new InvariantViolation(
-            'SomeObject lazy fields must be an associative array with field names as keys.'
-        ));
-
-        $objType->assertValid();
-    }
-
-    public function testReturningFieldsUsingYield(): void
-    {
-        $type = new ObjectType([
-            'name' => 'Query',
-            'fields' => static function (): \Generator {
-                yield 'url' => ['type' => Type::string()];
-                yield 'width' => ['type' => Type::int()];
-            },
-        ]);
-
-        $blogSchema = new Schema(['query' => $type]);
-
-        self::assertSame($blogSchema->getQueryType(), $type);
-
-        $field = $type->getField('url');
-        self::assertSame($field->name, 'url');
-        self::assertInstanceOf(StringType::class, $field->getType());
-
-        $field = $type->getField('width');
-        self::assertSame($field->name, 'width');
-        self::assertInstanceOf(IntType::class, $field->getType());
     }
 }
