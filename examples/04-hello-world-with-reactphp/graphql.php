7a78690e99548de8155f605772eecf615329ee16
spawnia <spawnia@users.noreply.github.com>
2022-11-16 14:05:36 +0000

    Apply php-cs-fixer changes

--- examples/04-hello-world-with-reactphp/graphql.php
+++ examples/04-hello-world-with-reactphp/graphql.php
@@ -11,7 +11,6 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use GraphQL\Error\DebugFlag;
 use GraphQL\Executor\ExecutionResult;
 use GraphQL\Executor\Promise\Adapter\ReactPromiseAdapter;
 use GraphQL\GraphQL;
