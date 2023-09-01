cc97e0e46bc3441074d1ea00d1562d02e421aab9
downace <downace@users.noreply.github.com>
2023-09-01 09:57:57 +0900

    Fix printing of single line descriptions with Unicode

--- src/Utils/SchemaPrinter.php
+++ src/Utils/SchemaPrinter.php
@@ -253,7 +253,7 @@ class SchemaPrinter
             : $indentation;
 
         if (count(Utils::splitLines($description)) === 1) {
-            $description = \json_encode($description, JSON_THROW_ON_ERROR);
+            $description = \json_encode($description, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
         } else {
             $description = BlockString::print($description);
             $description = $indentation !== ''
