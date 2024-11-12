31818bd9c74b34cb97a07260a9231c3e75952432
Simon Podlipsky <simon@podlipsky.net>
2024-11-12 11:51:02 +0100

    break cs

--- tests/Utils/CoerceInputValueTest.php
+++ tests/Utils/CoerceInputValueTest.php
@@ -216,8 +216,11 @@ final class CoerceInputValueTest extends TestCase
      */
     public function testReturnsNoErrorForAValidInput($input): void
     {
-        $result = Value::coerceInputValue($input, $this->testInputObject);
-        $this->expectGraphQLValue($result, ['foo' => 123]);
+
+        $result =       Value::coerceInputValue(
+            $input, $this->testInputObject,
+        );
+        $this->expectGraphQLValue($result, [  'foo' => 123]);
     }
 
     /** @return iterable<array{mixed}> */
