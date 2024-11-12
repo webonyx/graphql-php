05069b3863b768bff4072db5abd1a6505a0fb5d4
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-11-12 19:15:14 +0100

    Reset when override

--- src/Type/Definition/Type.php
+++ src/Type/Definition/Type.php
@@ -150,9 +150,7 @@ abstract class Type implements \JsonSerializable
      */
     public static function overrideStandardTypes(array $types): void
     {
-        $standardTypes = static::$standardTypes ?? [];
         self::reset();
-        static::$standardTypes = $standardTypes;
 
         foreach ($types as $type) {
             // @phpstan-ignore-next-line generic type is not enforced by PHP
