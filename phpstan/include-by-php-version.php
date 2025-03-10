046702433db05763f3633daa47576c51701524e9
Simon Podlipsky <simon@podlipsky.net>
2025-03-10 17:39:43 +0100

    <php82 baseline

--- phpstan/include-by-php-version.php
+++ phpstan/include-by-php-version.php
@@ -9,6 +9,9 @@ if (version_compare($phpversion, '8.4', '>=')) {
 if (version_compare($phpversion, '8.2', '>=')) {
     $includes[] = __DIR__ . '/php-at-least-8.2.neon';
 }
+if (version_compare($phpversion, '8.2', '<')) {
+    $includes[] = __DIR__ . '/php-below-8.2.neon';
+}
 if (version_compare($phpversion, '8.1', '<')) {
     $includes[] = __DIR__ . '/php-below-8.1.neon';
 }
