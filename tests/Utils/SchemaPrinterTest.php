d524cb9f08f7a6be1bc07b913da151719f9d04b6
Jason Bahl <jasonbahl@mac.com>
2025-07-08 11:56:39 -0600

    no message

--- tests/Utils/SchemaPrinterTest.php
+++ tests/Utils/SchemaPrinterTest.php
@@ -752,6 +752,28 @@ final class SchemaPrinterTest extends TestCase
         );
     }
 
+    /** @see it('Print Input Type with @oneOf directive') */
+    public function testInputTypeWithOneOfDirective(): void
+    {
+        $inputType = new InputObjectType([
+            'name' => 'InputType',
+            'isOneOf' => true,
+            'fields' => ['int' => ['type' => Type::int()]],
+        ]);
+
+        $schema = new Schema(['types' => [$inputType]]);
+
+        self::assertPrintedSchemaEquals(
+            <<<'GRAPHQL'
+            input InputType @oneOf {
+              int: Int
+            }
+
+            GRAPHQL,
+            $schema
+        );
+    }
+
     /** @see it('Custom Scalar') */
     public function testCustomScalar(): void
     {
