cf20ef32fad61638ff8f9a8e373c9180489cfffa
shmax <shmax@shmax.com>
2025-07-14 07:13:59 -0700

    formatting

--- src/Validator/ValidationCache.php
+++ src/Validator/ValidationCache.php
@@ -7,14 +7,15 @@ use GraphQL\Language\AST\DocumentNode;
 use GraphQL\Type\Schema;
 
 /**
- * Implement this interface and pass an instance to GraphQL::executeQuery to cache validation of ASTs. The details
- * of how to compute any keys (or whether to validate at all) are left up to you.
+ * Implement this interface and pass an instance to GraphQL::executeQuery to cache validation of ASTs.
+ * The details of how to compute any keys (or whether to validate at all) are left up to you.
  */
 interface ValidationCache
 {
     /**
      * Return true if the given schema + AST pair has previously been validated successfully.
-     * Only successful validations are cached. A return value of false means the pair is either unknown or has not been validated yet.
+     * Only successful validations are cached.
+     * A return value of false means the pair is either unknown or has not been validated yet.
      */
     public function isValidated(Schema $schema, DocumentNode $ast): bool;
 
