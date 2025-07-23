b4865e9974dd9b2f05952070a9ee0b653e7c4c20
shmax <shmax@shmax.com>
2025-07-22 18:55:05 -0700

    add comments about keys

--- src/Validator/ValidationCache.php
+++ src/Validator/ValidationCache.php
@@ -13,6 +13,14 @@ use GraphQL\Validator\Rules\ValidationRule;
  * This can improve performance by skipping validation for known-good query, schema, and rules combinations.
  * You are responsible for defining how cache keys are computed.
  *
+ * Some things to keep in mind when generating keys:
+ * - PHP's `serialize` method is fast, but can't handle certain structures such as closures.
+ * - If your `schema` does include closures or is quite large and complex, consider
+ *   using some kind of build-time version number or other environment variable.
+ * - Keep in mind that there are internal `rules` that are applied in addition to any you pass in,
+ *   and it's possible these may shift or expand as the library evolves, so it might make sense
+ *   to include the library version number in your keys.
+ *
  * @see PsrValidationCacheAdapter for a toy implementation.
  */
 interface ValidationCache
