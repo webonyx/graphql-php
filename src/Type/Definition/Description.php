13a1fffea0b43161387b0fd0229af5cee983dfcd
Benedikt Franke <benedikt@franke.tech>
2026-02-05 17:03:12 +0100

    fix: use PHP 7.4 compatible syntax in attribute classes (#1847)

--- src/Type/Definition/Description.php
+++ src/Type/Definition/Description.php
@@ -5,7 +5,10 @@ namespace GraphQL\Type\Definition;
 #[\Attribute(\Attribute::TARGET_ALL)]
 class Description
 {
-    public function __construct(
-        public string $description,
-    ) {}
+    public string $description;
+
+    public function __construct(string $description)
+    {
+        $this->description = $description;
+    }
 }
