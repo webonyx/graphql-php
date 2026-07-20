7dc0837b0d6a2ee55680164c73de7b301c81c6a4
Benedikt Franke <benedikt.franke@mll.com>
2026-07-20 12:20:53 +0200

    Apply make fix

--- src/Type/Introspection.php
+++ src/Type/Introspection.php
@@ -58,7 +58,6 @@ use GraphQL\Utils\Utils;
  * - typeIsOneOf
  *   Include field `isOneOf` for types?
  *   Default: false
- *
  * @see \GraphQL\Tests\Type\IntrospectionTest
  */
 class Introspection
